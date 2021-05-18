c04f11912fa53392057221ce9add629479597cbb
go <go.nobpadon25@gmail.com>
2021-05-18 16:08:37 +0700

    *

--- src/app/modules/_Demo/pages/QRGenerateDemo.js
+++ src/app/modules/_Demo/pages/QRGenerateDemo.js
@@ -32,7 +32,7 @@ function QRGenerateDemo() {
 			/>
 			<Grid item xs={12} md={12} lg={12} style={{ marginLeft: 10, marginTop: 10 }}>
 				<Link href="https://github.com/zpao/qrcode.react" color="textSecondary" target="_blank" rel="noopener">
-					pdfmake
+					QR
 			</Link>
 			</Grid>
 			<Grid item xs={12} md={12} lg={12} style={{ marginLeft: 10, marginTop: 10 }}>
