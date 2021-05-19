91bfd83dd6edd46fb015aa094b1641479e1f60da
go <go.nobpadon25@gmail.com>
2021-05-19 16:30:01 +0700

    **

--- src/app/modules/_Demo/components/reactToPrint/pageToPrint.js
+++ src/app/modules/_Demo/components/reactToPrint/pageToPrint.js
@@ -1,13 +1,14 @@
-import React from 'react'
-export class ComponentToPrint extends React.Component {
+/* eslint-disable no-restricted-imports */
+import React from 'react';
+export default class ComponentToPrint extends React.Component {
 
 	render() {
 		return (
-			<>
-				<div style={{ marginTop: 30 }}>
-					555
-				</div>
-			</>
+			<div style={{ marginTop: 30 }}>
+
+				{/* todo */}
+
+			</div>
 		);
 	}
 }
